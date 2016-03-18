7f1b7f893cd43e2c34a4625c89ce11d3824c5f0e
Brian Sheldon <brian.s.sheldon@gmail.com>
2016-03-18 16:50:14 +0000

    a

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ $google_api_config = [
   'application-id' => 'brainbucketb',
   'service-account-name' => 'brian.s.sheldon@developer.gserviceaccount.com',
   'private-key' => file_get_contents('brainBucketB-456d025fbf47.json'),
-  'dataset-id' => 'your-app-id'
+  'dataset-id' => 'brainbucketb'
 ];
