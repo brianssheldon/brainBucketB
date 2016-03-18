1069636b81964ba3c5b2658171fff6c73c44aca9
Brian Sheldon <brian.s.sheldon@gmail.com>
2016-03-18 15:41:19 +0000

    a

--- helloworld.php
+++ helloworld.php
@@ -1,1 +1,51 @@
-test test
+<html>
+ <head>
+ <title>My Form</title>
+ </head>
+ <body>
+ <form action="bad_word.php" method=post>
+ My name is:
+ <br> <input type="text" name="YourName"><br>
+ My favorite dirty word is:
+ <br><input type="text" name="FavoriteWord"> 
+ <p>
+ <input type="submit" name="submit" value="That's Right!">
+   <p>
+     <p>
+     <?php
+     	   $critters = array ( "Phoebe", "Ruby", "Rotty" );
+					foreach ($critters as &$value) {
+              print($value);
+              print("<p>");
+         }
+			?>use google\appengine\api\users\User;
+use google\appengine\api\users\UserService;
+# Looks for current Google account session
+$user = UserService::getCurrentUser();
+if ($user) {
+  echo 'Hello, ' . htmlspecialchars($user->getNickname());
+}
+else {
+  header('Location: ' . UserService::createLoginURL($_SERVER['REQUEST_URI']));
+}
+
+
+			?>
+       <p>
+    <?php
+      
+		use google\appengine\api\users\User;
+		use google\appengine\api\users\UserService;
+		# Looks for current Google account session
+		$user = UserService::getCurrentUser();
+		if ($user) {
+		  echo 'Hello, ' . htmlspecialchars($user->getNickname());
+		}
+		else {
+		  header('Location: ' . UserService::createLoginURL($_SERVER['REQUEST_URI']));
+		}   
+         
+    ?>
+ </form>
+ </body>
+ </html>
