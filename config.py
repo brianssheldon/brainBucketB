9288f0497b5989a020610d875017a3f59f651276
okieBubba <brian.s.sheldon@gmail.com>
2016-03-18 11:48:50 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 $google_api_config = [
-  'application-id' => 'brainBucketB',
-  'service-account-name' => 'xxx@developer.gserviceaccount.com',
-  'private-key' => file_get_contents('xxx-privatekey.p12'),
+  'application-id' => 'brainbucketb',
+  'service-account-name' => 'brian.s.sheldon@developer.gserviceaccount.com',
+  'private-key' => file_get_contents('brainBucketB-456d025fbf47.json'),
   'dataset-id' => 'your-app-id'
 ];
