1ce6a230825172d39cce323e5aac7eaaa605fbce
Brian Sheldon <brian.s.sheldon@gmail.com>
2016-03-18 16:20:25 +0000

    a

--- helloworld.php
+++ helloworld.php
@@ -15,3 +15,31 @@ else {
   header('Location: ' . UserService::createLoginURL($_SERVER['REQUEST_URI']));
 }
 // [END elseuser]
+>?
+  <p>
+  <html>
+ <head>
+ <title>My Form</title>
+ </head>
+ <body>
+ <form action="bad_word.php" method=post>
+ My name is:
+ <br> <input type="text" name="YourName"><br>
+ My favorite dirty word is:
+ <br><input type="text" name="FavoriteWord"> 
+ <p>
+ <input type="submit" name="submit" value="That's Right!">
+   <p>
+     <p>
+     <?php
+     	   $critters = array ( "Phoebe", "Ruby", "Rotty" );
+					foreach ($critters as &$value) {
+              print($value);
+              print("<p>");
+         }
+			?>
+       <p>
+       <p>
+ </form>
+ </body>
+ </html>
