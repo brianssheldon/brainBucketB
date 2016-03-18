c499226cb1167fd1e40d57faea49f74c7b987faa
Brian Sheldon <brian.s.sheldon@gmail.com>
2016-03-18 15:51:16 +0000

    a

--- helloworld.php
+++ helloworld.php
@@ -19,6 +19,7 @@
               print("<p>");
          }
 			?>
+       <p>
        <?php
        use google\appengine\api\users\User;
 			use google\appengine\api\users\UserService;
@@ -33,20 +34,6 @@
 
 			?>
        <p>
-    <?php
-      
-		use google\appengine\api\users\User;
-		use google\appengine\api\users\UserService;
-		# Looks for current Google account session
-		$user = UserService::getCurrentUser();
-		if ($user) {
-		  echo 'Hello, ' . htmlspecialchars($user->getNickname());
-		}
-		else {
-		  header('Location: ' . UserService::createLoginURL($_SERVER['REQUEST_URI']));
-		}   
-         
-    ?>
  </form>
  </body>
  </html>
