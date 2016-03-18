ba983a2156b6aa44173f7075a73ab96d2afda125
okieBubba <brian.s.sheldon@gmail.com>
2016-03-18 10:50:20 -0500

    Create bad_word.php

---
+++ bad_word.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Perv!</title>
+</head>
+
+<?php
+
+$YourName = $_REQUEST['YourName'] ;
+
+$FavoriteWord = $_REQUEST['FavoriteWord'] ;
+?>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<p>
+Hi <?php print $YourName; ?>
+<p>
+You like the word <b> <?php print $FavoriteWord; ?>!?!</b>
+
+<p>You oughta be ashamed of yourself!
+</body>
+</html>
